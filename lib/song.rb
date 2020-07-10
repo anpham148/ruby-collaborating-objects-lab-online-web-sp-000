29137866d1abdea43019103e059b29a71cab0da8
An Pham <anpham148@gmail.com>
2020-07-10 10:17:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,6 +12,6 @@ class Song
 
   def self.new_by_filename(filename)
     song = self.new(filename.split(" - ")[1])
-    song.artist.name = Artist.new(filename.split(" - ")[0]).add_song(song)
+    song.artist = Artist.new(filename.split(" - ")[0]).add_song(song)
   end
 end
