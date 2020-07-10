d2a858d3c82c87c81612bb9665381f5fcad6ba18
An Pham <anpham148@gmail.com>
2020-07-10 11:07:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,6 @@
 class Song
   attr_accessor :name, :artist
-  
+
   @@all = []
   def initialize(name)
     @name = name
@@ -11,7 +11,7 @@ class Song
     @@all
   end
 
-  def self.new_by_filename(filename)
+  def self.new_by_filename(filename)    
     song = self.new(filename.split(" - ")[1])
     song.artist = Artist.new(filename.split(" - ")[0])
     song
